75e0e41428501af5e392a8facf1d40e56199eed0
minjong kim <godminjong@naver.com>
2023-12-16 17:38:23 +0900

    feat: 출력 값 MessageFormatter로 구현

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,4 +1,30 @@
 package oncall.view;
 
+import static oncall.constant.ViewConstant.PRINT_DAILY_INFO_MESSAGE;
+import static oncall.constant.ViewConstant.PRINT_HOLIDAY_INFO_MESSAGE;
+
+import java.text.MessageFormat;
+import oncall.data.Calendar;
+import oncall.data.Day;
+
 public class OutputView {
+
+    public void renderAllSchedule(Calendar calendar){
+        calendar.getDays()
+                .stream()
+                .forEach(day -> printEachSchedule(day,calendar.getMonth().getOrder()));
+    }
+
+
+    private void printEachSchedule(Day day, int month){
+        if (day.isWeekDayAndHoliday()){
+            String format = MessageFormat.format(PRINT_HOLIDAY_INFO_MESSAGE,
+                    month,day.getDayValue(),day.getDayOfWeek().getNameOfDay() ,day.getMemberName());
+            System.out.println(format);
+            return;
+        }
+        String format = MessageFormat.format(PRINT_DAILY_INFO_MESSAGE,
+                month, day.getDayValue(), day.getDayOfWeek().getNameOfDay() ,day.getMemberName());
+        System.out.println(format);
+    }
 }
