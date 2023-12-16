ba857e1cd31847dc2a0e1ca353b56c8543243177
minjong kim <godminjong@naver.com>
2023-12-16 15:32:20 +0900

    feat: 날짜 정보 가지는 클래스 추가

--- src/main/java/oncall/data/Calendar.java
+++ src/main/java/oncall/data/Calendar.java
@@ -3,6 +3,8 @@ package oncall.data;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
 
 public class Calendar {
 
@@ -13,7 +15,7 @@ public class Calendar {
     private final List<Day> days = new ArrayList<>();
 
     public Calendar(int month, String startingDay) {
-         this.month = Arrays.stream(Month.values())
+        this.month = Arrays.stream(Month.values())
                 .filter(order -> order.getOrder().equals(month))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("예상치 못한 오류 입니다."));
@@ -21,15 +23,45 @@ public class Calendar {
                 .filter(day -> day.getNameOfDay().equals(startingDay))
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("예상치 못한 오류입니다."));
+        initializeCalendar();
+
+
     }
 
-    public void initializeCalendar(){
+    public void initializeCalendar() {
+        AtomicReference<Integer> dayOfWeekIndex = new AtomicReference<>(this.startingDay.getOrder());
+        Integer countOfDayInThisMonth = this.month.getDays();
 
+        IntStream.range(1, countOfDayInThisMonth)
+                .sequential()
+                .forEach(count -> addDayInfo(dayOfWeekIndex, count));
+
+        initializeHoliday();
     }
 
+    private boolean addDayInfo(AtomicReference<Integer> dayOfWeekIndex, int count) {
+        return days.add(new Day(count, false, DayOfWeek.findByOrder(
+                dayOfWeekIndex.getAndSet(dayOfWeekIndex.get() + 1))));
+    }
 
+    private void initializeHoliday() {
+        this.days
+                .stream()
+                .filter(this::checkHoliday)
+                .forEach(Day::updateToHoliday);
+        initializeWeekdayHoliday();
+    }
 
+    private void initializeWeekdayHoliday() {
+        if (this.month.getWeekDayHoliday() != null) {
+            month.getWeekDayHoliday()
+                    .forEach(e -> this.days.get(e - 1).updateToHoliday());
+        }
+    }
 
+    private boolean checkHoliday(Day day) {
+        return day.getDayOfWeek().equals(DayOfWeek.SUN) || day.getDayOfWeek().equals(DayOfWeek.SAT);
+    }
 
 
 }
--- src/main/java/oncall/data/DayOfWeek.java
+++ src/main/java/oncall/data/DayOfWeek.java
@@ -1,19 +1,40 @@
 package oncall.data;
 
-public enum DayOfWeek {
-    MON("월"),
-    TUE("화"),
-    WED("수"),
-    THU("목"),
-    FRI("금"),
-    SAT("토"),
-    SUN("일");
+import java.util.Arrays;
 
+public enum DayOfWeek {
+    MON("월",0),
+    TUE("화",1),
+    WED("수",2),
+    THU("목",3),
+    FRI("금",4),
+    SAT("토",5),
+    SUN("일",6);
+
+
+    private final Integer order;
+
+    public static DayOfWeek findByOrder(Integer order){
+        return Arrays.stream(DayOfWeek.values())
+                .filter(day -> day.order.equals(order))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("예상치 못한 오류입니다."));
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    DayOfWeek(Integer order, String nameOfDay) {
+        this.order = order;
+        this.nameOfDay = nameOfDay;
+    }
 
     private final String nameOfDay;
 
-    DayOfWeek(String nameOfDaya) {
+    DayOfWeek(String nameOfDaya, Integer order) {
         this.nameOfDay = nameOfDaya;
+        this.order = order;
     }
 
     public String getNameOfDay() {
