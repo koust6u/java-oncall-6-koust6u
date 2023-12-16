3661e8d3520dbfed9ec7f4ef56bae2b3a2d1fec9
minjong kim <godminjong@naver.com>
2023-12-16 13:48:01 +0900

    feat: 출력 상수 정의

---
+++ src/main/java/oncall/constant/ViewConstant.java
@@ -0,0 +1,11 @@
+package oncall.constant;
+
+public interface ViewConstant {
+    String INSERT_MONTH_INFO = "비상 근무를 배정할 월과 시작 요일을 입력하세요> ";
+    String INSERT_WEEKDAY_MEMBER_MESSAGE = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+    String INSERT_HOLIDAY_MEMBER_MESSAGE = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+
+    String PRINT_DAILY_INFO_MESSAGE = "{0}월 {1}일 {2} {3}";
+    String PRINT_HOLIDAY_INFO_MESSAGE = "{0}월 {1}월 {2}(휴일) {3}";
+
+}
